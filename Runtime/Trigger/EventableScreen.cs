e54d7fce96d504aa5c9b76af829e210f0efa33a9
UT_SIRT43 <utsirt43@163.com>
2024-08-29 23:55:13 +0800

    更改包名

--- Runtime/Trigger/EventableScreen.cs
+++ Runtime/Trigger/EventableScreen.cs
@@ -1,17 +1,16 @@
-using FTGAMEStudio.InitialSolution.Inputs;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace FTGAMEStudio.InitialSolution.EventableObject
 {
-    [AddComponentMenu("Initial Solution/Eventable Object/Eventable Screen"), RequireComponent(typeof(Camera))]
-    public class EventableScreen : EventableTrigger
+    [RequireComponent(typeof(Camera))]
+    public abstract class EventableScreen : EventableTrigger
     {
-        [Header("Pointer")]
-        public PointerEventInput input;
-
         private Ray ray;
         public override Ray Ray => ray;
+
+
+        protected abstract PointerEventData Data { get; }
 
         private new Camera camera;
 
@@ -19,11 +18,9 @@ namespace FTGAMEStudio.InitialSolution.EventableObject
 
         private void Update()
         {
-            PointerEventData pointer = input.GetValue();
-
-            if (pointer != null)
+            if (Data != null)
             {
-                ray = camera.ScreenPointToRay(pointer.position);
+                ray = camera.ScreenPointToRay(Data.position);
 
                 UpdateTrigger();
 
